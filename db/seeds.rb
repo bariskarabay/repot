9d581b38bca4acafdd541f248fd800330439c59c
Baris Karabay <bariskarabay@yandex.com>
2015-03-11 11:40:59 +0200

    Repot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Product.delete_all
+Product.create(title: 'Ruby ile programlama', 
+	description: 
+      % {<p>
+      	Ruby ile programlamaya giris bla bla bla bla bla
+      	</p>},
+      	image_url: 'resim.jpg',
+      	price: 49.98)
